e66b4ee433211395e5444222b73b650a28e46276
jungsunyoo <yoojungsun0@gmail.com>
2021-12-08 01:46:46 -0800

    fixing dtype issue

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -183,11 +183,11 @@ def wiener_like_rlddm_2step(np.ndarray[double, ndim=1] x1, # 1st-stage RT
 
 
 # uncomment for now and see what happens
-    # cdef double p
-    # cdef double sum_logp = 0
-    # cdef double wp_outlier = w_outlier * p_outlier
-    # cdef double alfa
-    # cdef double pos_alfa
+    cdef double p
+    cdef double sum_logp = 0
+    cdef double wp_outlier = w_outlier * p_outlier
+    cdef double alfa
+    cdef double pos_alfa
 
 
 
@@ -221,18 +221,17 @@ def wiener_like_rlddm_2step(np.ndarray[double, ndim=1] x1, # 1st-stage RT
     cdef np.ndarray[double, ndim=2] qs_mf = np.ones((comb(nstates,2,exact=True),2))*q # first-stage MF Q-values
     cdef np.ndarray[double, ndim=2] qs_mb = np.ones((nstates, 2))*q # second-stage Q-values
 
-    cdef np.ndarray[int, ndim=1] counter = np.zeros(comb(nstates,2,exact=True))
+    cdef np.ndarray[double, ndim=1] counter = np.zeros(comb(nstates,2,exact=True))
 
     cdef np.ndarray[double, ndim=1] Qmb
 
-# uncomment for now and see what happens
-    # cdef double dtq
-    # cdef double dtQ1
-    # cdef double dtQ2
-    # cdef double rt
+    cdef double dtq
+    cdef double dtQ1
+    cdef double dtQ2
+    cdef double rt
 
     cdef np.ndarray[double, ndim=2] Tm = np.array([[0.7, 0.3], [0.3, 0.7]]) # transition matrix
-    cdef np.ndarray[int, ndim=2] state_combinations = np.array(list(itertools.combinations(np.arange(nstates),2)))
+    cdef np.ndarray[double, ndim=2] state_combinations = np.array(list(itertools.combinations(np.arange(nstates),2)))
 
     if not p_outlier_in_range(p_outlier):
         return -np.inf
