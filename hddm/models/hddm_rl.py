f0b9b823c72f8a0f1b27432a952fb4f7823e29a5
jungsunyoo <yoojungsun0@gmail.com>
2022-01-28 23:06:34 -0800

    fix syntax error

--- hddm/models/hddm_rl.py
+++ hddm/models/hddm_rl.py
@@ -44,6 +44,8 @@ class HDDMrl(HDDM):
         self.dual = kwargs.pop("dual", False)
         self.alpha = kwargs.pop("alpha", True)
         self.gamma = kwargs.pop("gamma", True) # added for two-step task
+        m = kwargs['mfactor']
+        self.mfactor = m['freeparams']
         # mfactors = kwargs['mfactor']
         if 'lambda_' in self.mfactor:
             self.lambda_ = kwargs.pop("lambda_", True) # added for two-step task
